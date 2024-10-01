d695eb75b48187a91488257eab4c986d420e4090
Петрик Владислав <petrikvladislav17@gmail.com>
2024-10-01 13:10:27 +0300

    typlesdatas.java

--- typlesdatas.java
+++ typlesdatas.java
@@ -5,70 +5,70 @@ public class datatypes {
     public static void main(String[] args) {
         printdatatypes();
         Scanner scanner = new Scanner(System.in);
-        //int
-        System.out.println("Введіть ціле число (int): ");
+        //Числоль
+        System.out.println("Введеня числа (int): ");
         String intInput = scanner.nextLine();
         try {
             int intValue = Integer.parseInt(intInput);
             System.out.println("Введений int: " + intValue);
         } catch (NumberFormatException e) {
-            System.out.println("Неправильний формат для int.");
+            System.out.println("Неправильний int.");
         }
-        //double
-        System.out.println("Введіть число з плаваючою точкою (double): ");
+        //Число з точечкою
+        System.out.println("Введеня числа з точечкою (double): ");
         String doubleInput = scanner.nextLine();
         try {
             double doubleValue = Double.parseDouble(doubleInput);
             System.out.println("Введений double: " + doubleValue);
         } catch (NumberFormatException e) {
-            System.out.println("Неправильний формат для double.");
+            System.out.println("Неправильна точечка double.");
         }
-        //short
-        System.out.println("Введіть коротке число (short): ");
+        //Міні число
+        System.out.println("Введеня коротенького числа (short): ");
         String shortInput = scanner.nextLine();
         try {
             short shortValue = Short.parseShort(shortInput);
             System.out.println("Введений short: " + shortValue);
         } catch (NumberFormatException e) {
-            System.out.println("Неправильний формат для short.");
+            System.out.println("Неправильнийй short.");
         }
-        //long
-        System.out.println("Введіть довге число (long): ");
+        //Довжилезне число
+        System.out.println("Введеня довгого числа (long): ");
         String longInput = scanner.nextLine();
         try {
             long longValue = Long.parseLong(longInput);
             System.out.println("Введений long: " + longValue);
         } catch (NumberFormatException e) {
-            System.out.println("Неправильний формат для long.");
+            System.out.println("Неправильний long.");
         }
-        //float
-        System.out.println("Введіть число з плаваючою точкою (float): ");
+        //Число з крапочкою
+        System.out.println("Введеня числа з крапочкою (float): ");
         String floatInput = scanner.nextLine();
         try {
             float floatValue = Float.parseFloat(floatInput);
             System.out.println("Введений float: " + floatValue);
         } catch (NumberFormatException e) {
-            System.out.println("Неправильний формат для float.");
+            System.out.println("Неправильний float.");
         }
-        //char
-        System.out.println("Введіть символ (char): ");
+        //Іерогліф
+        System.out.println("Введеня символу (char): ");
         String charInput = scanner.nextLine();
         if (charInput.length() == 1) {
             char charValue = charInput.charAt(0);
             System.out.println("Введений char: " + charValue);
         } else {
-            System.out.println("Це не символ.");
+            System.out.println("Не той іерогліф.");
         }
-        //boolean
-        System.out.println("Введіть бульове знач. (true/false): ");
+        //Булінгове значеня
+        System.out.println("Введеня бульового значеня (true/false): ");
         String booleanInput = scanner.nextLine();
         boolean booleanValue = Boolean.parseBoolean(booleanInput);
         System.out.println("Введений boolean: " + booleanValue);
         scanner.close();
     }
-        //Інформація про примітивні типи даних
+        //Прімітівні типи даних
     public static void printdatatypes() {
-        System.out.println("Інформація про примітивні типи в Java:");
+        System.out.println("Про прітівівні типи в Javаa:");
         System.out.printf("%-10s | %-10s | %-20s | %-20s%n", "Тип", "Розмір (байти)", "Мін. знач.", "Макс. знач.");
         System.out.printf("%-10s | %-10d | %-20d | %-20d%n", "byte", 1, Byte.MIN_VALUE, Byte.MAX_VALUE);
         System.out.printf("%-10s | %-10d | %-20d | %-20d%n", "short", 2, Short.MIN_VALUE, Short.MAX_VALUE);
